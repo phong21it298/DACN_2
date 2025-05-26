586658c460b97354211ba397dc229e3a7e03053b
Tran Thanh Phong <phongtt.21it@vku.udn.vn>
2025-05-26 19:07:40 +0700

    fix ve QR code, scan

--- k_project_05/frontend/src/Dapp.tsx
+++ k_project_05/frontend/src/Dapp.tsx
@@ -5,6 +5,7 @@ import AddProductPage from "./pages/AddProductPage";
 import ScanProductPage from "./pages/ScanProductPage";
 import { WalletProvider } from "./context/WalletContext";
 import ProductListPage from "./pages/ProductListPage";
+import ProductDetailPage from "./pages/ProductDetailPage";
 
 export const Dapp = () => {
   return (
@@ -15,6 +16,7 @@ export const Dapp = () => {
         <Route path="/add-product" element={<AddProductPage />} />
         <Route path="/scan-product" element={<ScanProductPage />} />
         <Route path="/products" element={<ProductListPage />} />
+        <Route path="/product/:productId" element={<ProductDetailPage />} />
       </Routes>
     </WalletProvider>
   );
